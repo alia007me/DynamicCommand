34c9c12c572209772735d22c9602079b80bcff0d
Ali Lotfi <alia007.hi@gmail.com>
2021-02-06 19:35:29 +0330

    Init

---
+++ UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Commands;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register(DynamicCommand commands)
+        {
+            var test = commands.Commands[0] as LegalCommand;
+
+            var t = commands.Commands[0].GetType();
+
+            return Ok();
+        }
+    }
+}
