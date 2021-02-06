b60dabc0fe4c56c1137cb1872927c3043edce248
Ali Lotfi <alia007.hi@gmail.com>
2021-02-06 19:39:46 +0330

    Now Init

---
+++ WebApplication2/Commands/DynamicCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Commands
+{
+    public class DynamicCommand
+    {
+        public List<BaseCommand> Commands { get; set; }
+    }
+}
---
+++ WebApplication2/Commands/LegalCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Commands
+{
+    public class LegalCommand : BaseCommand
+    {
+        public int Age { get; set; }
+    }
+}
